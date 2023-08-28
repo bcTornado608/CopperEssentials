bb5ac09d1aaceb3d4764f02928266c207cd6822c
tianer2820 <tianer2820@outlook.com>
2023-08-27 20:03:49 -0400

    improve slime growing logic

--- src/main/java/com/github/tianer2820/copperessentials/tasks/SlimePotTask.java
+++ src/main/java/com/github/tianer2820/copperessentials/tasks/SlimePotTask.java
@@ -2,17 +2,15 @@ package com.github.tianer2820.copperessentials.tasks;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Slime;
@@ -24,82 +22,98 @@ import com.github.tianer2820.copperessentials.CopperEssentials;
 import com.github.tianer2820.copperessentials.utils.PotHelpers;
 
 public class SlimePotTask extends BukkitRunnable{
-    private Set<UUID> itemsInWater = new HashSet<>();
     private Random rng = new Random();
 
-    private static final int MAX_SLIME_SIZE = 16;
-    private static final int MAX_POT_SIZE = 256;
+    private static final int MAX_SLIME_SIZE = 126;
+    private static final int MAX_POT_SIZE = 512;
 
 
     @Override
     public void run() {
+        Set<Block> knownPotRegion = new HashSet<>();
+        
+        // get all items in water
         Set<Item> allItems = new HashSet<>();
         for (World world : CopperEssentials.getInstance().getServer().getWorlds()) {
             Collection<Item> items = world.getEntitiesByClass(Item.class);
             allItems.addAll(items);
         }
-        itemsInWater.retainAll(allItems.stream().map(Item::getUniqueId).collect(Collectors.toSet()));
-        allItems.forEach(this::updateItemInWater);
-    }
-
-    private void updateItemInWater(Item item){
-        if(item.getLocation().getBlock().getType() == Material.WATER){
-            // item in water
-            if(itemsInWater.contains(item.getUniqueId())){
-                // already in water, ignore
-            } else {
-                // just entered water, call trigger
-                processItemEnterWaterEvent(item);
-                itemsInWater.add(item.getUniqueId());
+        
+        // find all slime item stacks
+        Set<Item> allSlimeStacks = allItems.stream().filter(stack -> stack.getItemStack().getType() == Material.SLIME_BALL).collect(Collectors.toSet());
+        // check slime balls in a pot, try to make slimes
+        for (Item slimeStack : allSlimeStacks) {
+            Block stackBlock = slimeStack.getLocation().getBlock();
+            if(stackBlock.getType() != Material.WATER){
+                // not in water, no need for check
+                continue;
             }
-        } else {
-            // item not in water
-            itemsInWater.remove(item.getUniqueId());
+            if(!knownPotRegion.contains(stackBlock)){
+                // check if this is a new pot
+                Set<Block> potRegion = PotHelpers.detectPot(Material.COPPER_BLOCK, Material.WATER, stackBlock.getLocation().getBlock(), MAX_POT_SIZE);
+                knownPotRegion.addAll(potRegion);
+            }
+            if(knownPotRegion.contains(stackBlock)){
+                trySummonSlime(slimeStack);
+            }
+        }
+
+        // find all slimes, try to grow them
+        Set<Slime> allSlimes = new HashSet<>();
+        for (World world : CopperEssentials.getInstance().getServer().getWorlds()) {
+            Collection<Slime> items = world.getEntitiesByClass(Slime.class);
+            allSlimes.addAll(items);
+        }
+        // check slimes in a pot, try to grow slimes
+        for (Slime slime : allSlimes) {
+            Block slimeBlock = slime.getLocation().getBlock();
+            if(slimeBlock.getType() != Material.WATER){
+                // not in water, no need for check
+                continue;
+            }
+            if(!knownPotRegion.contains(slimeBlock)){
+                // check if this is a new pot
+                Set<Block> potRegion = PotHelpers.detectPot(Material.COPPER_BLOCK, Material.WATER, slimeBlock.getLocation().getBlock(), MAX_POT_SIZE);
+                knownPotRegion.addAll(potRegion);
+            }
+            if(knownPotRegion.contains(slimeBlock)){
+                tryGrowSlime(slime);
+            }
         }
     }
-    
-    private void processItemEnterWaterEvent(Item item){
-        ItemStack itemStack = item.getItemStack();
-        if (itemStack.getType() == Material.SLIME_BALL){
-            // slime ball entered water, detect copper pot and spawn slime
-            if (PotHelpers.detectPot(Material.COPPER_BLOCK, Material.WATER, item.getLocation().getBlock(), MAX_POT_SIZE).isEmpty()){
-                // not a pot, ignore
-            } else {
-                // is a pot, delete the slime ball, generate a slime
-                Location loc = item.getLocation();
-                ItemStack stack = item.getItemStack();
-                while (stack.getAmount() > 0) {
-                    Slime newSlime = (Slime)loc.getWorld().spawnEntity(loc, EntityType.SLIME, SpawnReason.CUSTOM);
-                    newSlime.setSize(0);
-                    stack.setAmount(stack.getAmount() - 1);
-                }
-                item.remove();
-            }
-        } else if (itemStack.getType() == Material.SUGAR){
-            // sugar entered water, detect copper pot and grow slime
-            Set<Block> potVolume = PotHelpers.detectPot(Material.COPPER_BLOCK, Material.WATER, item.getLocation().getBlock(), MAX_POT_SIZE);
-            if(potVolume.isEmpty()){
-                // not a pot, ignore
-            } else {
-                Collection<Slime> allSlimes = item.getWorld().getEntitiesByClass(Slime.class);
-                List<Slime> slimesInPot = allSlimes.stream()
-                        .filter(slime -> potVolume.contains(slime.getLocation().getBlock()) || potVolume.contains(slime.getLocation().getBlock().getRelative(BlockFace.DOWN)))
-                        .filter(slime -> slime.getSize() < MAX_SLIME_SIZE)
-                        .collect(Collectors.toList());
-                while(slimesInPot.size() > 0){
-                    int idx = rng.nextInt(0, slimesInPot.size());
-                    Slime luckySlime = slimesInPot.get(idx);
-                    luckySlime.setSize(luckySlime.getSize() + 1);
-                    ItemStack stack = item.getItemStack();
-                    stack.setAmount(stack.getAmount() - 1);
-                    if (stack.getAmount() == 0){
-                        item.remove();
-                        break;
-                    }
-                    if(luckySlime.getSize() >= 16){
-                        slimesInPot.remove(idx);
-                    }
-                }
+
+    private void trySummonSlime(Item slimeItem){
+        Optional<Item> nearBySugar = slimeItem.getWorld().getNearbyEntitiesByType(Item.class, slimeItem.getLocation(), 2.0)
+                .stream().filter(stack -> stack.getItemStack().getType() == Material.SUGAR).findAny();
+        
+        if(nearBySugar.isPresent()){
+            ItemStack sugarStack = nearBySugar.get().getItemStack();
+            sugarStack.setAmount(sugarStack.getAmount() - 1);
+
+            ItemStack slimeStack = slimeItem.getItemStack();
+            slimeStack.setAmount(slimeStack.getAmount() - 1);
+
+            Location loc = slimeItem.getLocation();
+            Slime newSlime = (Slime)loc.getWorld().spawnEntity(loc, EntityType.SLIME, SpawnReason.CUSTOM);
+            newSlime.setSize(1);
+        }
+    }
+
+    private void tryGrowSlime(Slime slime){
+        int oldSize = slime.getSize();
+        if(oldSize >= MAX_SLIME_SIZE){
+            return;
+        }
+
+        Optional<Item> nearBySugar = slime.getWorld().getNearbyEntitiesByType(Item.class, slime.getLocation(), 2.0)
+                .stream().filter(stack -> stack.getItemStack().getType() == Material.SUGAR).findAny();
+        
+        if(nearBySugar.isPresent()){
+            ItemStack sugarStack = nearBySugar.get().getItemStack();
+            sugarStack.setAmount(sugarStack.getAmount() - 1);
+
+            if(rng.nextDouble() < 1.0 / oldSize){
+                slime.setSize(oldSize + 1);
             }
         }
     }
